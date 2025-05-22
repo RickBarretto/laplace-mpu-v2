b46ec79750f948ea054caff9d2e53e081c913f57
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-22 14:54:09 -0300

    add helper functions + nil

--- app/types.h
+++ app/types.h
@@ -14,6 +14,14 @@
 typedef char* cstring;
 typedef struct { bool ok; } Status;
 
+Status ok() {
+    return (Status) { .ok = true };
+}
+
+Status err() {
+    return (Status) { .ok = false };
+}
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -23,5 +31,7 @@ typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
+
+#define nil (NULL)
 
 #endif
